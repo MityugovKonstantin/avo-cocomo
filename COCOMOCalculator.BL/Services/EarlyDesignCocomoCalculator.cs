a370f8a564561a0f8cab9a99d5898c086fe4d15b
Mityugov Konstantin <alxmira@yandex.ru>
2022-10-01 17:46:14 +0700

    Finished early design cocomo calculator

--- COCOMOCalculator.BL/Services/EarlyDesignCocomoCalculator.cs
+++ COCOMOCalculator.BL/Services/EarlyDesignCocomoCalculator.cs
@@ -1,14 +1,16 @@
-﻿using COCOMOCalculator.BL.Models;
+﻿using COCOMOCalculator.BL.Enums;
+using COCOMOCalculator.BL.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace COCOMOCalculator.BL.Services
 {
     public class EarlyDesignCocomoCalculator
     {
+        private readonly Dictionary<string, Dictionary<ScaleFactor, float>> _scale_factors = FileManager<ScaleFactor>.ScaleFactorDictionaryFill();
+        private readonly Dictionary<string, Dictionary<EarlyDesignEffortMultiplier, float>> _effort_multipliers =
+            FileManager<EarlyDesignEffortMultiplier>.EarlyDesignEffortMultiplierDictionaryFill();
+
         private const float A = 2.94f;
         private const float B = 0.91f;
         private const float C = 3.67f;
@@ -17,15 +19,56 @@ namespace COCOMOCalculator.BL.Services
         public CalculationResult Calculate(EarlyDesignCalculationArgs args)
         {
             var size = args.Size;
+            var scaleFactors = args.ScaleFactorsAttributes;
+            var effortMultipliers = args.EffortMultipliersAttributes;
 
+            var scaleFactorsSum =   ScaleFactorMap(nameof(scaleFactors.Precedentedness),                scaleFactors.Precedentedness);
+            scaleFactorsSum +=      ScaleFactorMap(nameof(scaleFactors.DevelopmentFlexibility),         scaleFactors.DevelopmentFlexibility);
+            scaleFactorsSum +=      ScaleFactorMap(nameof(scaleFactors.ArchitectureAndRiskResolution),  scaleFactors.ArchitectureAndRiskResolution);
+            scaleFactorsSum +=      ScaleFactorMap(nameof(scaleFactors.TeamCohesion),                   scaleFactors.TeamCohesion);
+            scaleFactorsSum +=      ScaleFactorMap(nameof(scaleFactors.ProcessMaturuty),                scaleFactors.ProcessMaturuty);
 
+            var e = B + 0.01f * scaleFactorsSum;
 
+            var eaf =   EffortMutriplierMap(nameof(effortMultipliers.PersonnelCapability),              effortMultipliers.PersonnelCapability);
+            eaf *=      EffortMutriplierMap(nameof(effortMultipliers.PersonnelExperience),              effortMultipliers.PersonnelExperience);
+            eaf *=      EffortMutriplierMap(nameof(effortMultipliers.ProductRelabilityAndComplexity),   effortMultipliers.ProductRelabilityAndComplexity);
+            eaf *=      EffortMutriplierMap(nameof(effortMultipliers.DeveloperForReusability),          effortMultipliers.DeveloperForReusability);
+            eaf *=      EffortMutriplierMap(nameof(effortMultipliers.PlatformDifficulty),               effortMultipliers.PlatformDifficulty);
+            eaf *=      EffortMutriplierMap(nameof(effortMultipliers.Facilities),                       effortMultipliers.Facilities);
+            eaf *=      EffortMutriplierMap(nameof(effortMultipliers.RequiredDevelopmentSchedule),      effortMultipliers.RequiredDevelopmentSchedule);
 
-            var eaf = 0f;
+            var peopleMonth = (float)Math.Round(eaf * A * Math.Pow(size, e), 6);
 
-            var peopleMonth = eaf * A * Math.Pow(size, 1);
-            //var timeMonth;
-            return null;// new CalculationResult() { PeopleMonth = peopleMonth, TimeMonth = timeMonth };
+            eaf =  EffortMutriplierMap(nameof(effortMultipliers.PersonnelCapability),               effortMultipliers.PersonnelCapability);
+            eaf *= EffortMutriplierMap(nameof(effortMultipliers.PersonnelExperience),               effortMultipliers.PersonnelExperience);
+            eaf *= EffortMutriplierMap(nameof(effortMultipliers.ProductRelabilityAndComplexity),    effortMultipliers.ProductRelabilityAndComplexity);
+            eaf *= EffortMutriplierMap(nameof(effortMultipliers.DeveloperForReusability),           effortMultipliers.DeveloperForReusability);
+            eaf *= EffortMutriplierMap(nameof(effortMultipliers.PlatformDifficulty),                effortMultipliers.PlatformDifficulty);
+            eaf *= EffortMutriplierMap(nameof(effortMultipliers.Facilities),                        effortMultipliers.Facilities);
+
+            var peopleMonthNs = eaf * A * Math.Pow(size, e);
+            var sced = EffortMutriplierMap(nameof(effortMultipliers.RequiredDevelopmentSchedule), effortMultipliers.RequiredDevelopmentSchedule);
+
+            var timeMonth = (float)Math.Round(sced * C * Math.Pow(peopleMonthNs, D + 0.2f * (e - B)), 6);
+
+            return new CalculationResult() { PeopleMonth = peopleMonth, TimeMonth = timeMonth };
+        }
+
+        private float ScaleFactorMap(string costAttribute, ScaleFactor scaleFactor)
+        {
+            _scale_factors.TryGetValue(costAttribute, out var dictionary);
+            if (!dictionary.TryGetValue(scaleFactor, out var coefficent))
+                throw new ArgumentException("Рейтинг аттрибута должен быть задан!");
+            return coefficent;
+        }
+
+        private float EffortMutriplierMap(string costAttribute, EarlyDesignEffortMultiplier earlyDesignEffortMultiplier)
+        {
+            _effort_multipliers.TryGetValue(costAttribute, out var dictionary);
+            if (!dictionary.TryGetValue(earlyDesignEffortMultiplier, out var coefficent))
+                throw new ArgumentException("Рейтинг аттрибута должен быть задан!");
+            return coefficent;
         }
     }
 }
